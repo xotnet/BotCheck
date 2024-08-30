dd6250c9e8252d22f66893841a8e13061d138d68
xotnet <150250186+xotnet@users.noreply.github.com>
2024-08-30 13:58:22 +0000

    Add files via upload

--- src/test.c
+++ src/test.c
@@ -1,1 +1,16 @@
+#include "botCheck.h"
+int main() {
+	char botCheckTask[64];
+	genBotCheckTask(botCheckTask, 12);
+	printf("%s\n", botCheckTask);
 
+	char solution[16];
+	passBotCheck(botCheckTask, solution);
+	printf("Solution is: %s\n", solution);
+
+	if (confirmBotCheckTask(botCheckTask, solution) == 1) {
+		printf("Client is't bot! :)\n");
+	} else {
+		printf("Client is bot! :(\n");
+	}
+}
